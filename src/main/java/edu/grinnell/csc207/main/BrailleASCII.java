f09ff84d71d2b6c47546df3af4bc97c51284a100
Jake Bell <3up3down10@gmail.com>
2024-12-03 22:53:42 -0600

    implement BrailleASCII

--- src/main/java/edu/grinnell/csc207/main/BrailleASCII.java
+++ src/main/java/edu/grinnell/csc207/main/BrailleASCII.java
@@ -2,6 +2,8 @@ package edu.grinnell.csc207.main;
 
 import java.io.PrintWriter;
 
+import edu.grinnell.csc207.util.BrailleAsciiTables;
+
 /**
  *
  */
@@ -15,8 +17,27 @@ public class BrailleASCII {
    */
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    // STUB
-    pen.close();
-  } // main(String[]
+      if (args.length != 2) {
+        pen.println("Incorrect arguement size");
+        pen.close();
+        return;
+      }
 
+      if (args[0].equals("braille")) {
+        String str = "";
+        for (char ch : args[1].toCharArray()) {
+          str += BrailleAsciiTables.toBraille(ch);
+        }
+        pen.println(str);
+      } else if (args[0].equals("ascii")) {
+        pen.println(BrailleAsciiTables.toAscii(args[1]));
+      } else if (args[0].equals("unicode")) {
+        String brail = "";
+        for (char ch : args[1].toCharArray()) {
+          brail += BrailleAsciiTables.toBraille(ch);
+        }
+        pen.println(BrailleAsciiTables.toUnicode(brail));
+      }
+    pen.close();
+  } // main(String[])
 } // class BrailleASCII
